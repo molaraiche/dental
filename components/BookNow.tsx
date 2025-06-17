b7ea5735d416aa1f9dcb27bddf06619fe754313e
molaraiche <laraichemohamed@gmail.com>
2025-06-17 15:23:26 +0100

    updates

--- components/BookNow.tsx
+++ components/BookNow.tsx
@@ -12,9 +12,9 @@ const BookNow = () => {
 
   return (
     <Modal>
-      <ModalTrigger className="bg-mid-blue-design text-secondary flex items-center justify-center gap-2 md:w-[280px] h-10">
-        <CiBookmark />
-        <span className="hidden md:flex">{t("book")}</span>
+      <ModalTrigger className="bg-mid-blue-design text-secondary flex items-center justify-center gap-3 py-2.5 px-6 cursor-pointer md:w-[260px]">
+        <CiBookmark className="w-6 h-6" />
+        <span className="hidden md:flex items-center justify-center">{t("book")}</span>
       </ModalTrigger>
 
       <ModalBody className="w-[80%] mx-10 rounded">
--- cypress/e2e/admin.cy.ts
+++
@@ -1,6 +0,0 @@
-describe("Login to admin", () => {
-  it("go to dental", () => {
-    cy.visit("http://localhost:3000/admin");
-    cy.get(".curosr-pointer").click();
-  });
-});
